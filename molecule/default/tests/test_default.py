b032b1c5da79b8cdf90b8cb7372749b03f918d72
Andrei Neustroev <aneustroev@naumen.ru>
2021-04-19 21:10:45 +0500

    Add tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,4 +1,5 @@
 import os
+import pytest
 
 import testinfra.utils.ansible_runner
 
@@ -13,3 +14,31 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+def test_app_working(host):
+    http = host.check_output('curl -L 127.0.0.1')
+
+    assert 'Hello World' in http
+
+
+def test_nginx_working(host):
+    http = host.check_output('curl -I -L 127.0.0.1')
+
+    assert '200 OK' in http
+
+
+def test_nginx_pkg(host):
+    pkg = host.package('nginx')
+
+    assert pkg.is_installed
+    assert '1.18.0' in pkg.version
+
+
+@pytest.mark.parametrize(
+    'path', [
+        ('/etc/nginx/conf.d/default.conf'),
+        ('/var/www/index.html'),
+    ])
+def test_minirole_files(host, path):
+    assert host.file(path).exists
